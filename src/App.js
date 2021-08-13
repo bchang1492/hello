720d200c9dd99708376bd1f8b7d953abe83cc1b0
bchang1492 <bchang1492@gmail.com>
2021-08-13 17:40:10 +0900

    9

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          bchang -- 7
+          bchang -- 9
         </p>
         <a
           className="App-link"
